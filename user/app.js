be45c2d7054fcf4477e7768ac103b715d36cf739
siddhu137 <siddhuabotula2@gmail.com>
2025-10-02 00:32:39 +0530

    first commit

---
+++ user/app.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const app = express();
+const userRoutes = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
+const connect = require("./db/db");
+connect();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use("/", userRoutes);
+module.exports = app;
